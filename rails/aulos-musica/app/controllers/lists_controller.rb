7e9080f56b03e4300e709b85a98ffa2f109babf2
polpols <paulgasser@aulos-musica.ch>
2015-04-15 18:58:52 +0200

    CRUD

--- rails/aulos-musica/app/controllers/lists_controller.rb
+++ rails/aulos-musica/app/controllers/lists_controller.rb
@@ -1,31 +1,43 @@
 class ListsController < ApplicationController
-
+  before_action :set_list, only: [:show,:edit,:update,:destroy]
   def index
-    @lists=List.all
+    @lists = List.all
   end
 
   def show
-    @list=List.find(params[:id])
+  end
+
+  def edit
   end
 
   def new
-    @list=List.new
-
+    @list = List.new
   end
 
   def create
-    @list=List.new
+    @list = List.new(list_params)
     respond_to do |format|
-      if @animal.save
-        format.html{redirect_to @list,notice:"List ws created"}
+      if @list.save
+        format.html{redirect_to @list,notice:"List was created"}
         format.json{}
       else
-
       end
-
     end
+  end
+
+  def update
+
   end
 
   def destroy
   end
+
+  private
+  def set_list
+      @list = List.find(params[:id])
+    end
+
+  def list_params
+      params.require(:list).permit(:name, :description)
+    end
 end
