ad9852fdcfd0e0121e9a94e25a5d34235fe63d77
polpols <paulgasser@aulos-musica.ch>
2015-04-19 09:41:30 +0200

    STI

--- rails/aulos-musica/app/models/partition.rb
+++ rails/aulos-musica/app/models/partition.rb
@@ -1,5 +1,4 @@
 class Partition < Product
   has_and_belongs_to_many :instruments, :join_table => "instrumentation", :foreign_key => "instrument_id"
   has_and_belongs_to_many :people, :join_table => "composership", :foreign_key => "contact_id"
-
 end
--- rails/aulos-musica/app/models/person.rb
+++ rails/aulos-musica/app/models/person.rb
@@ -1,7 +1,6 @@
 class Person < Contact
   has_and_belongs_to_many :products, :join_table => "composership", :foreign_key => "product_id"
 
-  attr_accessor :person_token
   def person_token
     self.name+", "+self.surname
   end
